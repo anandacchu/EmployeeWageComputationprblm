36d11c33b42ab819421ab764e7107d9732ab12dd
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-10-22 18:22:57 +0530

    Employeeattd

---
+++ EmployeePresentAbsent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_num1
+{
+    internal class EmpAttendance
+    {
+
+        public void EmpAttd()
+        {
+
+            int IS_PRESENT = 1;   // Constant
+            Random random = new Random(); // Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+        }
+    }
+}
